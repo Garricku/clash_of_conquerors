0b8a58d93fd837a0d9d1076497b3eae562d6ab08
Ricki Potts <ricki.glp@gmail.com>
2024-03-18 08:45:47 -0700

    updates to game

--- Clash Of Conquerors.py
+++ Clash Of Conquerors.py
@@ -6,38 +6,29 @@ import sys
 """These are imported modules used for game creation"""
 
 
-# Initialize Pygame
-pygame.init()
+win_width = 1920
+win_height = 1080
 
-# Load title screen
-bg_img = pygame.image.load('assets/title_screens/title_coc_2')
+class Main:
+    """This is the games main operations"""
+    def __init__(self):
+        """Initialize the Pygame"""
+        pygame.init()
+        
+        # Set game window
+        self.screen = pygame.display.set_mode((win_width, win_height))
+        pygame.display.set_caption("Clash Of Conquerors")
 
-# Set game window
-win_width = background_image.get_width()
-win_height = background_image.get_height()
-screen = pygame.display.set_mode((win_width, win_height))
-pygame.display.set_caption("Clash Of Conquerors")
+    def main_loop(self):
+        """This is the main game loop"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-# Load the custom mouse cursor
-cursor_img = pygame.image.load("assets/cursors/custom_cursor")
-pygame.mouse.set_visible(False) # Hides the default cursor
+        # Keep display updated
+        pygame.display.update()
 
-# Game loop
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    # Display the background image
-    screen.blit(bg_img, (0, 0))
-
-    # Display the custom cursor
-    cursor_rect = cursor_img.get_rect(center=pygame.mouse.get_pos())
-    screen.blit(cursor_img, cursor_rect)
-
-    pygame.display.flip()
-    clock.tick(60) # Frame rate limit
-
-pygame.quit()
-sys.exit()
+main = Main()
+main.main_loop()
